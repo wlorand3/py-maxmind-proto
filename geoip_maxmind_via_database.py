1dc16160018f1ca66f21dd7cdb13ce6503786fe7
Will Lorand <wlorand@cisco.com>
2022-03-19 10:27:55 -0700

    feat: added more ip addresses and loop thru them

--- geoip_maxmind_via_database.py
+++ geoip_maxmind_via_database.py
@@ -1,9 +1,11 @@
 import geoip2.database
-test_ip = '90.90.90.90'  # Paris
+import data.ip_data as data
 
 with geoip2.database.Reader('./db/GeoLite2-City_20220301/GeoLite2-City.mmdb') as reader:
-    response = reader.city(test_ip)
-    print(f'City Name: {response.city.name}')
-    print(
-        f'Lat/Lng: {response.location.latitude, response.location.longitude}')
-    print(f'Accuracy: {response.location.accuracy_radius} km')
+
+    for ip in data.more_ip_addresses:
+        ip_obj = reader.city(ip)
+        print(f'City Name: {ip_obj.city.name}')
+        print(
+            f'Lat/Lng: {ip_obj.location.latitude, ip_obj.location.longitude}')
+        print(f'Accuracy: {ip_obj.location.accuracy_radius} km\n')
