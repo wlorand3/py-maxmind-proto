eaadb50703055b8e0a160b90b60899d546d4119c
Will Lorand <wlorand@cisco.com>
2022-04-22 13:40:59 -0700

    successfully exported geojson and updated GeoLite2-City db

--- geoip_mm_database.py
+++ geoip_mm_database.py
@@ -10,9 +10,6 @@ with geoip2.database.Reader('./db/GeoLite2-City_20220301/GeoLite2-City.mmdb') as
     # for ip in data.more_ip_addresses:
     for ip in data.ip_address_pair:
         ip_obj = reader.city(ip)
-        # print(f'City Name: {ip_obj.city.name}')
-        # print(f'Lat/Lng: {ip_obj.location.latitude, ip_obj.location.longitude}')
-        # print(f'Accuracy: {ip_obj.location.accuracy_radius} km\n')
 
         # manually create a dict of the data and push to the geo_data list
         geo_data.append(
--- geoip_to_geojson.py
+++ geoip_to_geojson.py
@@ -1,8 +1,11 @@
 import geoip2.database
+from geojson import Point, Feature, FeatureCollection, dump
 
 import data.ip_data as data
 
-with geoip2.database.Reader('./db/GeoLite2-City_20220301/GeoLite2-City.mmdb') as reader:
+features = []
+
+with geoip2.database.Reader('./db/GeoLite2-City_20220419/GeoLite2-City.mmdb') as reader:
 
     # for ip in data.more_ip_addresses:
     for ip in data.ip_address_pair:
@@ -11,3 +14,14 @@ with geoip2.database.Reader('./db/GeoLite2-City_20220301/GeoLite2-City.mmdb') as
         print(
             f'Lat/Lng: {ip_obj.location.latitude, ip_obj.location.longitude}')
         print(f'Accuracy: {ip_obj.location.accuracy_radius} km\n')
+
+        # write geojson feature obj
+        features.append(Feature(geometry=Point((ip_obj.location.longitude, ip_obj.location.latitude)), properties={
+                        "geoip_accuracy_radius": ip_obj.location.accuracy_radius, "city_name": ip_obj.city.name}))
+
+# outside of the db file reading loop, create a geojson FeatureCollection
+feature_collection = FeatureCollection(features)
+
+# write the feature collection to a .geojson file
+with open('./geojson/geoip_results.geojson', 'w') as geojson_file:
+    dump(feature_collection, geojson_file)
